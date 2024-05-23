4f3c9c0b3cf8c9fcd7ff6938c7bd0e83006152d0
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-24 00:47:01 +0900

    feat: define user entity

---
+++ src/main/java/com/example/springsecurityseminar/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.springsecurityseminar.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column
+	private String phone;
+
+	@Builder
+	public User(String username, String name, String password, String phone) {
+		this.username = username;
+		this.name = name;
+		this.password = password;
+		this.phone = phone;
+	}
+}
