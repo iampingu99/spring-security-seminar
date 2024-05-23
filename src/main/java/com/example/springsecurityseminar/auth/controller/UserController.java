901dc0539c7af252cb1f6b8fc28abfdd19dc158e
Jinwoo Lee <binaryWooo@gmail.com>
2024-05-24 05:14:30 +0900

    feat: add appendix

--- src/main/java/com/example/springsecurityseminar/auth/controller/UserController.java
+++ src/main/java/com/example/springsecurityseminar/auth/controller/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springsecurityseminar.auth.service.UserService;
+import com.example.springsecurityseminar.util.JwtUtil;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -15,9 +17,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/user")
 public class UserController {
 	private final UserService userService;
+	private final JwtUtil jwtUtil;
 
 	@GetMapping("/{id}")
-	public ResponseEntity<?> read(@PathVariable Long id) {
+	public ResponseEntity<?> read(HttpServletRequest request, @PathVariable Long id) {
+		Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
+		if (!userId.equals(id)) {
+			return ResponseEntity.badRequest().build();
+		}
 		return ResponseEntity.ok(userService.read(id));
 	}
 }
